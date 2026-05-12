22e6fca160f42140413c7940e70f66a869364020
MMMalca <m.meyer@alcamobil.de>
2026-05-12 16:58:14 +0200

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 
 // @ts-ignore
 export default defineConfig({
-  base: process.env.GITHUB_PAGES === 'true' ? '/lol-pro-meta-tool/' : '/',
+  base: '/',
   plugins: [react()],
   test: {
     environment: 'node',
